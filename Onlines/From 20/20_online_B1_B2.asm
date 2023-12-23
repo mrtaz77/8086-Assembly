7d1764f16db3f5d99fe4b431da193abbbc9fa540
mrtaz77 <mrtazwar23@gmail.com>
2023-12-23 11:06:31 +0600

    b1b2 online done

---
+++ Onlines/From 20/20_online_B1_B2.asm
@@ -0,0 +1,243 @@
+.MODEL SMALL ;SCOPE OF CODE
+
+.STACK 100H ;ALLOCATE MEMORY IN HEXADECIMAL
+
+.DATA ;VARIABLE DECLARATION
+CR EQU 0DH
+LF EQU 0AH
+
+NL DB CR,LF,'$'
+ERROR DB CR,LF,'Invalid input$'   
+
+STR1 DB 'uyhitnae$'
+STR2 DB 'qwertykeyboards$'
+STR3 DB 'eruiaaageruiaaag$'
+
+VOWEL_NUM DB 'Vowel Count: $'
+CONS_NUM DB 'Consonant Count: $'
+
+; EQU MEANS EQUAL
+; THESE CONSTANTS ARE USED IN ALL CODES
+; USED FOR NEW LINE
+
+.CODE
+
+MAIN PROC ; LIKE MAIN
+
+;DATA SEGMENT INIT
+
+MOV AX, @DATA;BRINGS DATA TO AX FOR INIT
+MOV DS, AX
+;THE ABOVE PART IS SAME FOR MOST CODES  
+
+;code here
+
+LEA SI,STR1
+MOV CX,8
+CALL NUM_VOWEL_CONSONANT
+
+CALL NEWLINE
+CALL NEWLINE
+
+LEA SI,STR2
+MOV CX,15
+CALL NUM_VOWEL_CONSONANT
+
+CALL NEWLINE
+CALL NEWLINE
+
+
+LEA SI,STR3
+MOV CX,16
+CALL NUM_VOWEL_CONSONANT
+
+CALL EXIT
+
+
+
+
+
+ERR: 
+LEA DX,ERROR
+CALL PRINT_STR
+INT 21H
+
+CALL EXIT
+
+
+MAIN ENDP          
+
+PROC NUM_VOWEL_CONSONANT NEAR
+    MOV AX,0
+    MOV BX,0 
+    
+    MOV DL,[SI]
+    MOV DL,0
+    
+    
+   COUNTER:
+    CMP BYTE PTR[SI],'a'
+    JE VOWEL
+    
+    CMP BYTE PTR[SI],'e'
+    JE VOWEL 
+    CMP BYTE PTR[SI],'i'
+    JE VOWEL
+    CMP BYTE PTR[SI],'o'
+    JE VOWEL
+    CMP BYTE PTR[SI],'u'
+    JE VOWEL          
+    
+    INC BX
+    
+    JMP INCREMENT
+    
+   VOWEL:
+    INC AX
+    
+    
+    
+   INCREMENT:
+    ADD SI,1
+    
+       
+       
+    LOOP COUNTER 
+    
+    PUSH BX
+    PUSH AX
+    
+    LEA DX,VOWEL_NUM
+    CALL PRINT_STR
+    
+    POP AX
+    
+    CALL PRINT_NUM
+    
+    CALL NEWLINE
+    
+    LEA DX,CONS_NUM
+    CALL PRINT_STR
+    
+    POP AX
+    
+    CALL PRINT_NUM
+    
+    RET 
+    
+
+;print single char
+
+PROC INPUT_CHAR NEAR
+    MOV AH,1
+    INT 21H
+    RET
+
+
+PROC PRINT_CHAR NEAR
+     MOV AH,2
+     INT 21H
+     RET   
+
+;outputs n(stored in cx) numbers as output and stores in array address
+
+PROC OUTPUT_ARR_NUM NEAR
+    OUTPUT_ARR_NUM1:
+        MOV AX,WORD PTR[SI]
+        CALL PRINT_NUM
+        ADD SI,2       
+        CALL NEWLINE
+        LOOP OUTPUT_ARR_NUM1
+        RET
+
+
+
+;takes n(stored in cx) numbers as input and stores in the array address
+;of SI
+
+PROC INPUT_ARR_NUM NEAR
+    INPUT_ARR_NUM1:
+        CALL INPUT_NUM
+        MOV WORD PTR[SI],AX
+        ADD SI,2
+        LOOP INPUT_ARR_NUM1
+        RET   
+    INPUT_ARR_NUM ENDP
+
+PROC PRINT_STR NEAR
+    MOV AH,9
+    INT 21H
+    RET       
+
+PROC NEWLINE NEAR
+    LEA DX,NL
+    CALL PRINT_STR  
+    RET             
+    
+PROC EXIT NEAR
+    MOV AH,4CH
+    INT 21H
+    RET        
+    
+;takes a decimal number input and stores it in AX register 
+;Number is space terminated 
+   
+PROC INPUT_NUM NEAR 
+        MOV BX,0
+    INP:
+        MOV AH,1
+        INT 21H    
+        
+        CMP AL,' '
+        JE EXIT_INPUT
+        CMP AL,'0'
+        JB ERR
+        CMP AL,'9'
+        JA ERR
+        
+        SUB AL,30H
+        AND AX,0FFH
+        PUSH AX
+        MOV AL,0AH
+        MUL BX
+        MOV BX,AX
+        POP AX  
+        ADD BX,AX
+        MOV AH,1
+        JMP INP
+    EXIT_INPUT:
+        MOV AX,BX
+        RET 
+    
+    
+
+; prints the number in decimal stored in AX register
+
+PROC PRINT_NUM NEAR
+    MOV BX,0AH
+    PUSH 404H ; end of number
+    ;DISPLAY NUM
+    NUM_IN_STACK:
+        CMP BX,AX
+        JA EXIT_NUM_STACK
+        MOV DX,0
+        DIV BX
+        PUSH DX
+        JMP NUM_IN_STACK
+    EXIT_NUM_STACK:
+        CMP AX,0
+        JE NO_PUSH
+        PUSH AX
+    NO_PUSH:
+        MOV AH,2
+    DISPLAY:
+        POP DX
+        CMP DX,404H
+        JE EXIT_PRINT
+        ADD DX,30H
+        INT 21H
+        JMP DISPLAY
+    EXIT_PRINT:
+        RET
+
+END MAIN;END OF CODE
--- README.md
+++ README.md
@@ -14,12 +14,29 @@ Codes,offlines and onlines using 8086 assembly programming.
 
 - Offlines
     - Offline 1
-        - [Specification](/Offlines/Offline1/July_2023_CSE_316_Assembly_Offline_1.pdf)
+        - [Spec](/Offlines/Offline1/July_2023_CSE_316_Assembly_Offline_1.pdf)
         - [2nd_largest_char](/Offlines/Offline1/2nd_largest_char.asm)
         - [classify_char](/Offlines/Offline1/classify_char.asm)
     
     - Offline 2
-        - [Specification](/Offlines/Offline2/Assembly_Offline_2.pdf)
+        - [Spec](/Offlines/Offline2/Assembly_Offline_2.pdf)
         - [chocolate_num](/Offlines/Offline2/chocolate_num.asm)
         - [sum_of_digits](/Offlines/Offline2/sum_of_digits.asm)
 
+- Onlines 
+    - From 20
+        - Online1
+            - [Spec](/Onlines/From%2020/Assembly%20Online%20A1%20A2.pdf)
+            - [checkArraySorting](/Onlines/From%2020/20_online_A1_A2.asm)
+
+    - From 19
+        - Online1
+            - [Spec](/Onlines/From%2019/Online%201%20Questions.txt)
+            - [min_of_multiple_lowercase_letters](/Onlines/From%2019/Online1/19_online1_B1.asm)
+            - [hash_of_two_strings](/Onlines/From%2019/Online1/19_online1_B2.asm)
+            - [ternary_to_binary](/Onlines/From%2019/Online1/19_online1_A1.asm)
+            - [gcd](/Onlines/From%2019/Online1/19_online1_A2.asm)
+        
+        - Online2
+            - [Spec](/Onlines/From%2019/Online%202%20Questions.txt)
+            - [max_length_increasing_subarray](/Onlines/From%2019/Online2/maxLengthSubArray.asm)
